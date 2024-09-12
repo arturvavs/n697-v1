1d111150b14332551c501adca78050b89dee6c12
Artur Araujo <arturavs1996@gmail.com>
2024-09-11 22:52:14 -0300

    ajuste tratativa de erro

--- src/app/pages/user_page/UserPage.tsx
+++ src/app/pages/user_page/UserPage.tsx
@@ -24,7 +24,6 @@ export const UserPager: React.FC = () => {
   const [sortColumn, setSortColumn] = useState<keyof (typeof pets)[0]>("nomePet");
   const [sortDirection, setSortDirection] = useState<"asc" | "desc">("asc");
   const [pets, setPets] = useState<any[]>([]);
-  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchPets = async () => {
@@ -66,7 +65,7 @@ export const UserPager: React.FC = () => {
         sexoPet: pet.get("sexoPet") || "",
       }));
     } catch (err) {
-      throw new Error(`Erro ao buscar pets:`);
+      alert(`Erro ao buscar pets:`);
     }
   };
   {/*Filtra os registros considerando alguns atributos, a partir da digitação com tratativa para converter a string para LowerCase */}
